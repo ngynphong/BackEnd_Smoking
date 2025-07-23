cbe571577ae49732ed49a4af9dbf7bb253f243ca
ngynphong <124499195+ngynphong@users.noreply.github.com>
2025-07-23 14:50:37 +0700

    feat: update chat routes to include 'plus' subscription access

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -6,15 +6,15 @@ const { checkSubscriptionAccess } = require('../middlewares/SubscriptionMiddlewa
 // All routes in this file are protected and require authentication
 
 // Route to start a new chat
-chatRouter.post('/', validateToken, checkSubscriptionAccess(['premium']), chatController.startChat);
+chatRouter.post('/', validateToken, checkSubscriptionAccess(['plus','premium']), chatController.startChat);
 
 // Route to send a message in a chat
-chatRouter.post('/:chatId/message', validateToken, checkSubscriptionAccess(['premium']), chatController.sendMessage);
+chatRouter.post('/:chatId/message', validateToken, checkSubscriptionAccess(['plus', 'premium']), chatController.sendMessage);
 
 // Route to get the history of a chat
-chatRouter.get('/:chatId', validateToken, checkSubscriptionAccess(['premium']), chatController.getChatHistory);
+chatRouter.get('/:chatId', validateToken, checkSubscriptionAccess(['plus', 'premium']), chatController.getChatHistory);
 
 // Route to get all chat sessions for a user
-chatRouter.get('/sessions/:userId', validateToken, checkSubscriptionAccess(['premium']), chatController.getChatSessions);
+chatRouter.get('/sessions/:userId', validateToken, checkSubscriptionAccess(['plus', 'premium']), chatController.getChatSessions);
 
 module.exports = chatRouter;
